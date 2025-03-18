477be5f55214603f6e93feeda8505165f495faca
Matteo Cederle <matteita99@gmail.com>
2025-03-18 17:57:24 +0100

    run evaluation scripts in aquarium

--- gnn/fuel_new/evaluation_base.sh
+++ gnn/fuel_new/evaluation_base.sh
@@ -3,7 +3,7 @@
 # Script to reproduce results
 for ((s=1;s<11;s+=1))
 do
-  python evaluation_run.py \
+  CUDA_VISIBLE_DEVICES=2 python evaluation_run.py \
   --seed $s \
   --file_name continuous_base_fixom \
   --nn_architecture base
--- gnn/fuel_new/evaluation_smart.sh
+++ gnn/fuel_new/evaluation_smart.sh
@@ -3,7 +3,7 @@
 # Script to reproduce results
 for ((s=1;s<11;s+=1))
 do
-  python evaluation_run.py \
+  CUDA_VISIBLE_DEVICES=2 python evaluation_run.py \
   --seed $s \
   --file_name continuous_smart_fixom \
   --nn_architecture smart
