95afd2ace619018643e6fe03712071b49e5654c6
matteita99 <matteita99@gmail.com>
2025-03-24 12:00:34 +0100

    finished evaluation

--- gnn/mixed_new/evaluation.sh
+++ gnn/mixed_new/evaluation.sh
@@ -3,7 +3,7 @@
 # Script to reproduce results
 for ((s=1;s<8;s+=6))
 do
-  python evaluation_run.py \
+  CUDA_VISIBLE_DEVICES=2 python evaluation_run.py \
   --seed $s \
   --file_name continuous_smart \
   --nn_architecture smart
